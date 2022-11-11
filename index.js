d3b1dd5a4dc02a1050d97fddb9a003dfd3d2b6e4
mccallw23 <william.l.mccall.23@dartmouth.edu>
2022-11-10 23:22:23 -0500

    eliminate body parser double define

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ const app = express();
 app.use(cors({
     origin: '*'
 }));
-var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: "50mb"}));
 app.use(bodyParser.urlencoded({limit: "50mb", extended: true, parameterLimit:50000}));
 app.use(express.json())
